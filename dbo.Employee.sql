c10cb5c4b9cee4f9f8f167f3161117036c45666f
Muhammad Faham <fahambbs@gmail.com>
2025-03-17 12:46:10 -0700

    Add files via upload

---
+++ dbo.Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [ID]              INT           NOT NULL,
+    [Name]            VARCHAR (100) NOT NULL,
+    [Email]           NCHAR (50)    NOT NULL,
+    [Gender]          VARCHAR (100) NOT NULL,
+    [Date of Joining] VARCHAR (50)  NOT NULL,
+    [Profle Picture]  VARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
